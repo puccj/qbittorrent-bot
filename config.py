03c8411391d0b5e4994fda1b1238ef58ace190eb
puccj <daniele.pucci2@studio.unibo.it>
2025-10-06 19:24:18 +0200

    Create config example

---
+++ config.py
@@ -0,0 +1,14 @@
+BOT_TOKEN = "token_from_BotFather"
+QBITTORRENT_HOST = "127.0.0.1"
+QBITTORRENT_PORT = 8080
+QBITTORRENT_USER = "admin"
+QBITTORRENT_PASS = "adminadmin"
+
+# Authorized users (Telegram user IDs)
+AUTHORIZED_USERS_ID = [123456788,
+                       123456789]
+
+DOWNLOAD_PATHS = {
+    "film": "/home/user/Download/film",
+    "tv": "/home/user/Download/serieTV",
+}
